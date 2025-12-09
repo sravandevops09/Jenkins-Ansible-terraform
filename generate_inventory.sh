22ff35817609feb3b694c2e7a865200462939a61
sravandevops09 <sravankumar.devops09@gmail.com>
2025-12-09 12:57:26 +0530

    Update generate_inventory.sh

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -5,12 +5,11 @@ ubuntu_linux_ip=$(terraform output -raw ubuntu_linux_public_ip)
 
 cat <<EOF > inventory.ini
 [frontend]
-${amzn_linux_ip} ansible_user=ec2-user
+${amzn_linux_ip} ansible_host=${amzn_linux_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/${AMAZON_KEY_PAIR}.pem
 
 [backend]
-${ubuntu_linux_ip} ansible_user=ubuntu
+${ubuntu_linux_ip} ansible_host=${ubuntu_linux_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/${UBUNTU_KEY_PAIR}.pem
 EOF
 
 echo "Generated inventory.ini"
 cat inventory.ini
-
